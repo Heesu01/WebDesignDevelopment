e5fdf4a38635079e759765a475fc2af6041df6a4
Heesu01 <rulru0413@naver.com>
2025-03-22 13:51:09 +0900

    기출 04회: 요구사항 구현

--- exams/exam04/javascript/scrip.js
+++ exams/exam04/javascript/scrip.js
@@ -1,3 +1,41 @@
 jQuery(document).ready(function () {
-  //
+  // 메뉴
+  $("nav>ul>li").mouseover(function () {
+    $(this).children(".submenu").stop().slideDown(500);
+  });
+  $("nav>ul>li").mouseout(function () {
+    $(this).children(".submenu").stop().slideUp(500);
+  });
+  // 이미지슬라이드
+  setInterval(function () {
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginTop: -300 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginTop: -600 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginTop: 0 });
+    $(".slidelist").delay(2000);
+  });
+
+  //탭메뉴
+  $(".tabcontents > .notice").show();
+  $(".tabcontents > .gallery").hide();
+
+  $(".tabmenu li").click(function () {
+    var idx = $(this).index();
+
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+
+    $(".tabcontents > div").hide();
+    $(".tabcontents > div").eq(idx).show();
+  });
+
+  //모달
+  $(".notice li:first").click(function () {
+    $(".modal").fadeIn(0.5);
+  });
+  $(".btn").click(function () {
+    $(".modal").fadeOut(0.5);
+  });
 });
