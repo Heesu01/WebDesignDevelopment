fd608161be4f6fee029395cda8f4934d409e75f3
Heesu01 <rulru0413@naver.com>
2025-03-22 16:45:13 +0900

    기출 05회: 요구사항 구현

--- exams/exam05/javascript/script.js
+++ exams/exam05/javascript/script.js
@@ -1,3 +1,28 @@
 jQuery(document).ready(function () {
-  //
+  // 메뉴
+  $("nav>ul>li").mouseover(function () {
+    $(this).children(".submenu").stop().slideDown(500);
+  });
+  $("nav>ul>li").mouseout(function () {
+    $(this).children(".submenu").stop().slideUp(500);
+  });
+
+  // 이미지 슬라이드
+  setInterval(function () {
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -800 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -1600 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: 0 });
+    $(".slidelist").delay(2000);
+  });
+
+  //모달
+  $(".notice li:first").click(function () {
+    $(".popup").fadeIn(0.5);
+  });
+  $(".btn").click(function () {
+    $(".popup").fadeOut(0.5);
+  });
 });
