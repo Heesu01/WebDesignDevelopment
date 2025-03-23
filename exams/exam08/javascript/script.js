eb6abb995030cbd034a2ddc5eb2f4b184ca95919
Heesu01 <rulru0413@naver.com>
2025-03-23 20:13:36 +0900

    기출 08회: 요구사항 구현

--- exams/exam08/javascript/script.js
+++ exams/exam08/javascript/script.js
@@ -1,3 +1,29 @@
 jQuery(document).ready(function () {
-  //
+  // 메뉴
+  $(".submenu").hide();
+  $("nav > ul ").mouseover(function () {
+    $(".submenu").stop().slideDown(500);
+  });
+  $("nav > ul ").mouseout(function () {
+    $(".submenu").stop().slideUp(500);
+  });
+
+  //이미지 슬라이드
+  setInterval(function () {
+    $(".slide-list").delay(2000);
+    $(".slide-list").animate({ marginTop: -400 });
+    $(".slide-list").delay(2000);
+    $(".slide-list").animate({ marginTop: -800 });
+    $(".slide-list").delay(2000);
+    $(".slide-list").animate({ marginTop: 0 });
+    $(".slide-list").delay(2000);
+  });
+
+  // 모달
+  $(".notice ul li:first").click(function () {
+    $(".modal").fadeIn(100);
+  });
+  $(".modal button").click(function () {
+    $(".modal").fadeOut(100);
+  });
 });
