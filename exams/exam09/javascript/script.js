5a0032f91eeacad8fefbee17520c61bd23f417c8
Heesu01 <rulru0413@naver.com>
2025-03-24 00:53:04 +0900

    기출 09회: 요구사항 구현

--- exams/exam09/javascript/script.js
+++ exams/exam09/javascript/script.js
@@ -1,3 +1,42 @@
 jQuery(document).ready(function () {
-  //
+  // 메뉴
+  $("nav>ul>li").mouseover(function () {
+    $(this).children(".submenu").stop().slideDown(500);
+  });
+  $("nav>ul>li").mouseout(function () {
+    $(this).children(".submenu").stop().slideUp(500);
+  });
+
+  // 이미지 슬라이드
+  setInterval(function () {
+    $(".slide-list").delay(2000);
+    $(".slide-list").animate({ marginTop: -400 });
+    $(".slide-list").delay(2000);
+    $(".slide-list").animate({ marginTop: -800 });
+    $(".slide-list").delay(2000);
+    $(".slide-list").animate({ marginTop: 0 });
+    $(".slide-list").delay(2000);
+  });
+
+  // 탭메뉴
+  $(".notice");
+  $(".gallery").hide();
+
+  $(".tab-menu > li ").click(function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+
+    var idx = $(this).index();
+
+    $(".tab-contents>div").hide();
+    $(".tab-contents>div").eq(idx).show();
+  });
+
+  // 모달
+  $(".notice ul li:first").click(function () {
+    $(".modal").show();
+  });
+  $(".modal button").click(function () {
+    $(".modal").hide();
+  });
 });
