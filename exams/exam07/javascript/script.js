49a84488c148c468b23fa4d52ee096b3f861377b
Heesu01 <rulru0413@naver.com>
2025-03-23 15:20:39 +0900

    기출 07회: 요구사항 구현

--- exams/exam07/javascript/script.js
+++ exams/exam07/javascript/script.js
@@ -1,3 +1,40 @@
 jQuery(document).ready(function () {
-  //
+  // 메뉴
+  $("nav ul li").mouseover(function () {
+    $(this).find(".submenu").stop().slideDown();
+  });
+  $("nav ul li").mouseout(function () {
+    $(this).find(".submenu").stop().slideUp();
+  });
+
+  // 이미지슬라이드
+  $(".img-slide a").hide();
+  $(".img-slide a:first").show();
+  setInterval(function () {
+    $(".img-slide a:first")
+      .fadeOut(1000)
+      .next("a")
+      .fadeIn(1000)
+      .end()
+      .appendTo(".img-slide");
+  }, 3000);
+
+  //탭메뉴
+  $(".tab-contents .notice").show();
+  $(".tab-contents .gallery").hide();
+  $(".tab-menu li").click(function () {
+    var idx = $(this).index();
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    $(".tab-contents > div").hide();
+    $(".tab-contents > div").eq(idx).show();
+  });
+
+  // 모달
+  $(".notice li:first").click(function () {
+    $(".modal").show();
+  });
+  $(".btn").click(function () {
+    $(".modal").hide();
+  });
 });
